d70666b09d11df5c2ec4662e28f838168e6fabef
Jonah Kornberg <kornberg.jonah@gmail.com>
2022-12-22 21:52:36 -0500

    loading indicator on login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,57 @@
-import { Button, Card, Divider, Typography } from '@mui/material'
-import { green } from '@mui/material/colors'
+import { Button, Card, colors, Divider, Typography } from '@mui/material'
+import { green, red } from '@mui/material/colors'
 import { Box } from '@mui/system'
 import { useRouter } from 'next/router'
 import useWindowSize from '../common/hooks/useWindowSize'
 //create landing page
 const Landing = () => {
-  const width = useWindowSize()
-  const router = useRouter()
+    const width = useWindowSize()
+    const router = useRouter()
 
-  return (
-    <Card
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        backgroundImage: 'url(/cabin_landing.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <Typography variant='h2'>Welcome to holifi</Typography>
-      <Typography variant='h6'>
-        Turn your favorite song lyrics into a fun holiday parody!
-      </Typography>
-      <Divider
-        light={true}
-        variant={'fullWidth'}
-        style={{
-          margin: '20px 0 20px 0',
-          width: '400px',
-          backgroundColor: 'white',
-          height: '0.5px',
-          border: 'none',
-        }}
-      />
-      <Button
-        style={{ marginTop: '0' }}
-        variant='contained'
-        color='primary'
-        onClick={() => router.push('/login')}
-      >
-        Join the Party
-      </Button>
-    </Card>
-  )
+    return (
+        <Card
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                // backgroundImage: 'url(/cabin_landing.png)',
+                backgroundColor: green[300],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <Box>
+                <Typography variant='h2' display={'inline'}>Welcome to </Typography>
+                <Typography variant='h1' display={'inline'} color={green}>holi</Typography>
+                <Typography variant='h1' display={'inline'} color={red[400]}>fi</Typography>
+
+            </Box>
+            <Typography variant='h6'>
+                Turn your favorite song lyrics into a fun holiday parody!
+            </Typography>
+            <Divider
+                light={true}
+                variant={'fullWidth'}
+                style={{
+                    margin: '20px 0 20px 0',
+                    width: '400px',
+                    backgroundColor: 'white',
+                    height: '2px',
+                    border: 'none',
+                }}
+            />
+            <Button
+                style={{ marginTop: '0' }}
+                variant='contained'
+                color='primary'
+                onClick={() => router.push('/login')}
+            >
+                Join the Party
+            </Button>
+        </Card>
+    )
 }
 
 export default Landing
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Stack, Input, Link, Typography, Card, Snackbar, Alert, Container, Box, FormControlLabel, Checkbox, Grid, TextField } from '@mui/material'
+import { Button, FormControl, FormLabel, Stack, Input, Link, Typography, Card, Snackbar, Alert, Container, Box, FormControlLabel, Checkbox, Grid, TextField, CircularProgress } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { Router, useRouter } from 'next/router'
@@ -41,6 +41,9 @@ const Login = (props: Props) => {
         })
     }
   });
+
+  const [loading, setLoading] = useState(true);
+
   const { login, loadingUser, signInWithGoogle } = useAuth();
   const mounted = useMounted()
   const router = useRouter()
@@ -51,82 +54,89 @@ const Login = (props: Props) => {
   useEffect(() => {
     if (loadingUser.user != null && loadingUser.isLoading == false) {
       router.push('/app')
+    } else if (loadingUser.isLoading == false) {
+      setLoading(false)
     }
-  }, [loadingUser.user])
+  }, [loadingUser])
 
   return (
     <Container maxWidth={'sm'} component="main"><Snackbar open={open}><Alert>'Credentials not valid.'</Alert></Snackbar>
-      <form onSubmit={formik.handleSubmit}>
-        <Box
-          component="div"
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'stretch',
-          }}
-        >
 
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <FormControl id='email'>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="email"
-              label='Email Address'
-              autoComplete="email"
-              autoFocus
-              type='email'
-              onChange={e => formik.setFieldValue('email', e.target.value)}
-            />
-          </FormControl>
-          <FormControl id='password'>
-            <TextField
-              name='password'
-              type='password'
-              label='Password'
-              autoComplete='password'
-              fullWidth
-              required
-              onChange={e => formik.setFieldValue('password', e.target.value)}
-            />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" checked={checked}
-              onChange={(e) => setChecked(e.target.checked)} color="secondary" />}
+      {loading ? <CircularProgress /> :
+        <>
+          <form onSubmit={formik.handleSubmit}>
+            <Box
+              component="div"
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'stretch',
+              }}
+            >
 
-            label="Remember me"
-          />
-          <LoadingButton
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            loading={isSubmitting}
-            type='submit'
-          >
-            Sign in
-          </LoadingButton>
-          <Stack>
-            <Link href="/forgot" variant="body2">
-              Forgot password?
-            </Link>
-            <Link href="/signup" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Stack>
-        </Box>
-      </form>
-      <Box display={'flex'} marginTop={5}>
-        <Box margin='0 auto' display={'inline-block'}>
-          <GoogleButton
-            onClick={() => { signInWithGoogle()}}
-          />
-        </Box>
-      </Box>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <FormControl id='email'>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="email"
+                  label='Email Address'
+                  autoComplete="email"
+                  autoFocus
+                  type='email'
+                  onChange={e => formik.setFieldValue('email', e.target.value)}
+                />
+              </FormControl>
+              <FormControl id='password'>
+                <TextField
+                  name='password'
+                  type='password'
+                  label='Password'
+                  autoComplete='password'
+                  fullWidth
+                  required
+                  onChange={e => formik.setFieldValue('password', e.target.value)}
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="remember" checked={checked}
+                  onChange={(e) => setChecked(e.target.checked)} color="secondary" />}
+
+                label="Remember me"
+              />
+              <LoadingButton
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                loading={isSubmitting}
+                type='submit'
+              >
+                Sign in
+              </LoadingButton>
+              <Stack>
+                <Link href="/forgot" variant="body2">
+                  Forgot password?
+                </Link>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Stack>
+            </Box>
+          </form>
+          <Box display={'flex'} marginTop={5}>
+            <Box margin='0 auto' display={'inline-block'}>
+              <GoogleButton
+                onClick={() => { signInWithGoogle() }}
+              />
+            </Box>
+          </Box>
+        </>
 
 
+      }
     </Container>
   )
 }
