be407fa1b72454edc99ed5ad3dbfd283fa51e435
Jonah Kornberg <kornberg.jonah@gmail.com>
2022-12-24 14:52:26 -0500

    add analytics to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,25 @@ import { ThemeProvider } from '@mui/material/styles'
 
 import AuthContextProvider from '../contexts/AuthContext'
 import { CssBaseline } from '@mui/material'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={app_theme}>
       <CssBaseline />
       <AuthContextProvider>
-        <Component {...pageProps} />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-C6PWCWMMSP" />
+        <Script id='google-analytics' strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C6PWCWMMSP', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}/>
+          <Component {...pageProps} />
       </AuthContextProvider>
     </ThemeProvider>
   )
